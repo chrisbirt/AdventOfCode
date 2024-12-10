d938f37de9678395a4dfac100dbf268b690682ae
Chris Birt <chris.birt@gmail.com>
2024-12-10 17:35:32 +0000

    Day 7 refactored

--- AoC_2024/day07.py
+++ AoC_2024/day07.py
@@ -2,15 +2,16 @@ from aoc import AoC
 from itertools import product
 import re
 
+# refactored from original solution to not use pythons eval method
+
 def solve(input):
     p1, p2 = 0, 0
 
-    # '190: 10 19' becomes ['190', '10', '19']
-    input = [[int(row[0])] + row[1:] for row in (re.split(': | ', row) for row in input)]
+    # '190: 10 19' becomes [190, 10, 19]
+    input = [[int(r) for r in row] for row in (re.split(': | ', row) for row in input)]
 
     def is_valid(row, operands):
-        result = row[0]
-        numbers = row[1:]
+        result, numbers = row[0], row[1:]
 
         # pre-calc operand combinations. All combinations of length one less than the number of numbers
         ocs = [o for o in list(product(operands, repeat=len(numbers)-1)) if len(o) == (len(numbers)-1)]
@@ -19,9 +20,11 @@ def solve(input):
             total = int(numbers[0])
             for i, operand in enumerate(oc):
                 if operand == '|':
-                    total = int(str(total) + numbers[i + 1])
-                else:
-                    total = eval(str(total) + operand + numbers[i + 1])
+                    total = int(str(total) + str(numbers[i + 1]))
+                elif operand == '*':
+                    total *= numbers[i + 1]
+                elif operand == '+':
+                    total += numbers[i + 1]
             if total == result:
                 return True
         return False
@@ -36,8 +39,3 @@ def solve(input):
 day = 7
 puzzle_input = AoC.load_puzzle_input(day)
 solve(puzzle_input)
-
-# solve here
-#2941973819040
-#249943041417600
-
