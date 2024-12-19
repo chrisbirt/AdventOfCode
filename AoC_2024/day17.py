08625c7e992e8b050aa08df5cd4ced1ac4d00f7b
Chris Birt <chris.birt@gmail.com>
2024-12-19 07:30:09 +0000

    Day 17 - tidied explanation

--- AoC_2024/day17.py
+++ AoC_2024/day17.py
@@ -103,41 +103,44 @@ def solve(input):
 
     # By reverse-engineering the program, we can see the code executes as follows:
     #
-    # Program       instruction             substituted
-    # 1: 2, 4       b = a % 8               b = a % 8
-    # 2: 1, 5       b = b ^ 5               b = (a % 8) ^ 5
-    # 3: 7, 5       c = int(a / (2 ** b))   c = int(a / (2 ** ((a % 8) ^ 5)))
-    # 4: 4, 5       b = b ^ c               b = ((a % 8) ^ 5) ^ (int(a / (2 ** ((a % 8) ^ 5))))
-    # 5: 0, 3       a = int(a / (2 ** 3))   a = int(a / 8)
-    # 6: 1, 6       b = b ^ 6               b = (((a % 8) ^ 5) ^ (int(a / (2 ** ((a % 8) ^ 5))))) ^ 6
-    # 7: 5, 5       print(b % 8)            print(((((a % 8) ^ 5) ^ (int(a / (2 ** ((a % 8) ^ 5))))) ^ 6) % 8)
-    # 8: 3, 0       if a != 0 goto 0        if a != 0 goto 0
+    # Program       instruction             
+    # 1: 2, 4       b = a % 8               
+    # 2: 1, 5       b = b ^ 5               
+    # 3: 7, 5       c = int(a / (2 ** b))   
+    # 4: 4, 5       b = b ^ c               
+    # 5: 0, 3       a = int(a / (2 ** 3))   <= THIS IS DECREMENTING A EACH LOOP
+    # 6: 1, 6       b = b ^ 6               
+    # 7: 5, 5       print(b % 8)            <= B IS ULTIMATELY A FUNCTION OF A. b = ((((a % 8) ^ 5) ^ (int(a / (2 ** ((a % 8) ^ 5))))) ^ 6) % 8
+    # 8: 3, 0       if a != 0 goto 0        <= THIS CONTROLS LOOPING/ENDING
     #
-    # The approach is to work back from the last value in the program, working out what value of reg_ will give the last value, 
-    # then the last 2 values, then the last 3 values etc, working back.
+    # My approach is to work back from the last value in the program, working out what values of reg_A will give the last value, 
+    # then the last 2 values, then the last 3 values etc.
+    
+    # The key observation is that when int(reg_A / 2 ** 3)=0,  the program will have outputted a 0 (the last value in the program) and then ends.
+    # So what value of reg_A will output a value of 0? It must be between 0 and 7 (since if reg_A > 7, the program won't end yet.)
     #
-    # The key observation is that when int(reg_a/8)=0 the program will have outputted 0 (the last value in the program) and then ends.
-    # So what value of reg_a will output a value of 0? It must be between 0 and 7 (since if reg_a > 7, the program won't end yet.)
-    #
-    # When reg_a = 3, the program outputs 0.
+    # I find that when reg_A = 3, the program outputs 0.
     print(run(3, reg_b, reg_c, prog))
 
-    # Then, what value of reg_a will produce int(reg_a/8)=3, and also output [3, 0]
-    # reg_a must be a value between 24 and 31.
-    # when a = 24 or when a = 31, we get an outut of [3, 0]
+    # Next, what value of reg_A will produce int(reg_A / 2 ** 3)=3, and also output [3, 0]
+    # reg_A must be a value between 24 and 31.
+    #
+    # I find that when reg_a = 24 or when reg_A = 31, we get an outut of [3, 0]
     print(run(24, reg_b, reg_c, prog))
     print(run(31, reg_b, reg_c, prog))
 
-    # Then, what values of reg_a will produce int(reg_a/8)=24 or 31, and also output [5, 3, 0]
-    # a must be a value between 192 and 199 or between 248 and 255.
-    # when a = 192, 196, 198, 199 or 249, we get an output of [5, 3, 0]
+    # Then, what values of reg_a will produce int(reg_a / 2 ** 3)=24 or 31, and also output [5, 3, 0]
+    # reg_A must be a value between 192 and 199 or between 248 and 255.
+    #
+    # I find that when reg_A = 192, 196, 198, 199 or 249, we get an output of [5, 3, 0]
     print(run(192, reg_b, reg_c, prog))
     print(run(196, reg_b, reg_c, prog))
     print(run(198, reg_b, reg_c, prog))
     print(run(199, reg_b, reg_c, prog))
     print(run(249, reg_b, reg_c, prog))
 
-    # Generalise the approach, and loop until the starting value of reg_a is found
+    # Generalising the approach, loop until the starting value of reg_A is found that produces an output that
+    # matches the input
 
     seeds = [0]
     
@@ -146,6 +149,7 @@ def solve(input):
         for s in seeds:
             for reg_a in range(s * 8, (s+1) * 8 ):
                 output = run (reg_a, reg_b, reg_c, prog)
+                # does the output match the end of the program?
                 if output == prog[-len(output):]:
                     new_seeds.append(reg_a)
         seeds = new_seeds
